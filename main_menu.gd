409b043b43ddfd567980c909400b01e457111b79
Juan Jose Vergara <40677444+jjvemz@users.noreply.github.com>
2024-08-18 23:56:12 -0400

    Pause menu created

---
+++ main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+
+
+func _on_play_pressed() -> void:
+	pass # Replace with function body.
+
+
+
+func _on_level_selector_pressed() -> void:
+	pass # Replace with function body.
+
+
+
+func _on_exit_pressed() -> void:
+	pass # Replace with function body.
