566ff5ba2b6351380610fa4600029c8a45052b2c
Andrés Cerda Pimentel <12897630+a-cerda@users.noreply.github.com>
2024-08-20 19:20:02 -0400

    kljwehgflkhjg

--- scenes/finish.gd
+++ scenes/finish.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@export var next_level: PackedScene
 
 func _on_body_entered(body):
-	pass # Replace with function body.
+	if body.has_method("respawn"):
+		get_tree().change_scene_to_packed(next_level)
